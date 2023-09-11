c0c0cac1cc458eb97e49b1a3ef7d7ae02f8b1503
Robert Jett <me@robertjett.com>
2023-09-11 13:56:56 +0200

    Added japanese language.

--- pages/index.js
+++ pages/index.js
@@ -106,7 +106,7 @@ const MyBlog = ({ posts }) => {
       </div>
       <div className="scrolling-text-container">
         <div className="scrolling-text">
-          THIS IS A TEST OF THE NEW WORLD PERSON CONTENT SERVICE. THIS IS ONLY A TEST.
+        これはNEW WORLD PERSONコンテンツサービスのテストです。これはテストだけです。
         </div>
       </div>
       <div className="container" ref={containerRef}>
